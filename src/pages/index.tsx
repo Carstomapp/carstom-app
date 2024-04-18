ca5c6e972704f60fa354de6294894d47f4919bfe
laksby <laksby@icloud.com>
2024-04-18 21:59:25 +0200

    Corrected mobile application height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { SplashLogo } from '../components';
 
 export const IndexPage: FC = () => {
   return (
-    <main className="tw-h-screen tw-flex tw-flex-col tw-items-stretch tw-justify-center">
+    <main className="tw-h-dvh tw-flex tw-flex-col tw-items-stretch tw-justify-center">
       <SplashLogo />
     </main>
   );

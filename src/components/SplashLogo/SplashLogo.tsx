961f59de1f52381561fa56239f72c9a3e4823f6a
laksby <laksby@icloud.com>
2024-04-26 02:19:31 +0200

    Added splash screen initial animation. First version

--- src/components/SplashLogo/SplashLogo.tsx
+++ src/components/SplashLogo/SplashLogo.tsx
@@ -2,12 +2,24 @@ import { FC } from 'react';
 
 export const SplashLogo: FC = () => {
   return (
-    <div className="tw-relative">
-      <img className="tw-max-h-dvh tw-w-full tw-h-auto tw-object-contain" src="images/logo_back.png" />
+    <div className="tw-relative tw-overflow-hidden">
       <img
-        className="tw-absolute tw-max-h-dvh tw-w-full tw-h-auto tw-object-contain tw-top-1/2 tw-translate-y-[-50%]"
-        src="images/logo_front.png"
+        className="tw-max-h-dvh tw-w-full tw-h-auto tw-object-contain tw-animate-splash-logo-background"
+        src="images/logo_back.png"
       />
+      <div className="tw-absolute tw-flex tw-items-center tw-w-full tw-top-1/2 tw-translate-y-[-50%] tw-animate-splash-logo-foreground-open">
+        <img className="tw-max-h-dvh tw-w-full tw-h-auto tw-object-contain" src="images/logo_front.png" />
+        <div
+          className="tw-absolute tw-inset-0"
+          style={{
+            mask: 'url(images/logo_front.png)',
+            maskSize: 'contain',
+          }}>
+          <div className="tw-flex tw-justify-end tw-items-center tw-w-full tw-h-full tw-opacity-0 tw-animate-splash-logo-shine">
+            <div className="tw-bg-white tw-rounded-full tw-w-[20vw] tw-h-[20vw] tw-mr-[10vw]" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,46 @@ module.exports = {
   prefix: 'tw-',
   important: false,
   content: ['src/components/**/*.{ts,tsx}', 'src/pages/**/*.{ts,tsx}', 'src/styles/**/*.{css}'],
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        'splash-logo-background': 'splash-logo-background 500ms ease-in-out',
+        'splash-logo-foreground-open': 'splash-logo-foreground-open 500ms ease-in-out',
+        'splash-logo-shine': '500ms splash-logo-shine 500ms ease-in-out',
+      },
+      keyframes: {
+        'splash-logo-background': {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        'splash-logo-foreground-open': {
+          '0%': {
+            opacity: '0',
+            transform: 'translate(100%, -50%)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translate(0, -50%)',
+          },
+        },
+        'splash-logo-shine': {
+          '0%': {
+            opacity: '0',
+            transform: 'rotate(0)',
+          },
+          '50%': {
+            opacity: '0.5',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+    },
+  },
 };

70f3da7e5c542f76de45053b76a5344eb002fe8b
laksby <laksby@icloud.com>
2024-09-01 21:59:44 +0200

    Added API initialization logging

--- src/api/axios/client.ts
+++ src/api/axios/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import { Logger } from '../../utils/Logger';
 import { QueryString } from '../../utils/QueryString';
 
 const token = QueryString.getString('t');
+
+Logger.info(`API initialization: ${process.env.API_URL}`);
 
 export const client = axios.create({
   baseURL: process.env.API_URL,
